126df8143568e63230ba22bbf501030a4ec923be
sinofseven <em.s.00001@gmail.com>
2023-09-03 06:32:52 +0900

    impl

---
+++ terraform/env/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.5.4"
+
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 4.13.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.CLOUDFLARE_API_TOKEN
+}
+
+module "common" {
+  source = "../module/common"
+}
+
+
+variable "CLOUDFLARE_API_TOKEN" {}
---
+++ terraform/module/common/access_idp.tf
@@ -0,0 +1,13 @@
+resource "cloudflare_access_identity_provider" "auth0" {
+  name = "auth0"
+  type = "oidc"
+  account_id = data.cloudflare_accounts.current.id
+
+  config {
+    client_id = "Ghx3kMR7xeW7GDJwgL0LftJDDwgMKyEB"
+    client_secret = "i7ZjnD13q1gqxpVRrB_5i60oS5Nm-g_WzdzwjAR3p9SK1iZY24EkOGqR7MzYR-Co"
+    auth_url = "https://luciferous-private.jp.auth0.com/authorize"
+    token_url = "https://luciferous-private.jp.auth0.com/oauth/token"
+    certs_url = "https://luciferous-private.jp.auth0.com/.well-known/jwks.json"
+  }
+}
