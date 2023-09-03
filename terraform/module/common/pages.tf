034def87f83e42daba92503eac18b384ae9bfc2e
sinofseven <em.s.00001@gmail.com>
2023-09-04 03:15:56 +0900

    add pages

---
+++ terraform/module/common/pages.tf
@@ -0,0 +1,20 @@
+resource "cloudflare_pages_project" "ryza" {
+  account_id        = local.account_id
+  name              = "ryza"
+  production_branch = "master"
+
+  build_config {
+    build_command = "npm run build"
+    destination_dir = "dist"
+  }
+
+  source {
+    type = "github"
+    config {
+      production_branch = "master"
+      owner = "sinofseven"
+      repo_name = "tools-of-atelier-of-ryza"
+      deployments_enabled = true
+    }
+  }
+}
